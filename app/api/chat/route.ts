62c1d117a2ba0c3d890f97c8101905a74b6c4643
Andrew Miracle <me@andrewmiracle.com>
2023-09-15 19:37:38 +0000

    failed conversationalQA implementation

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -7,6 +7,9 @@ import { prompt } from './prompt';
 import { connect, OpenAIEmbeddingFunction } from 'vectordb'
 import { ChatOpenAI } from 'langchain/chat_models/openai'
 import { BytesOutputParser } from 'langchain/schema/output_parser';
+import { LanceDB } from "langchain/vectorstores/lancedb";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { ConversationalRetrievalQAChain } from "langchain/chains";
 
 
 const embeddings = new OpenAIEmbeddingFunction('pageContent', process.env.OPENAI_API_KEY as string);
@@ -58,6 +61,10 @@ export async function POST(req: Request) {
         member: `chat:${id}`
       })
 
+  const currentMessageContent = messages[messages.length - 1].content;
+
+  
+
 
    /**
    * Vector Retrieval step for our actor
@@ -65,40 +72,57 @@ export async function POST(req: Request) {
    * And use it to perform a similarity search on LanceDB
    **/
     const db = await connect('/Users/apple/srv/koolamusic/rag/data');
-    const table = await db.openTable('investors', embeddings);
+    // const table = await db.openTable('investors', embeddings);
+
+
+    const table = await db.openTable("investors");
   
+    const vectorStore = new LanceDB(new OpenAIEmbeddings(), { table });
+  
+
+
+
     /**
      * We need to filter out the messages that are not tweets
      * or quote tweets when executing our queries
      *
      * @see https://lancedb.github.io/lancedb/sql/
      */
-    const results = await table
-      // .search("")
-      .search('I need AI investors')
-      .limit(3)
-      .execute();
+    // const results = await table
+    //   .search(currentMessageContent)
+    //   .limit(3)
+    //   .execute();
   
-    // need to make sure our prompt is not larger than max size
-    const formattedContext = results
-      .map((r) => r.pageContent)
-      .join('\n\n---\n\n')
-      .substring(0, 3750);
+    // // need to make sure our prompt is not larger than max size
+    // const formattedContext = results
+    //   .map((r) => r.pageContent)
+    //   .join('\n\n---\n\n')
+    //   .substring(0, 3750);
   
 
 
 
-      console.log({
-        formattedContext,
-      })
+    //   console.log({
+    //     formattedContext,
+    //   })
 
 
-        /**
+    let streamedResponse = "";
+
+    /**
    * See a full list of supported models at:
    * https://js.langchain.com/docs/modules/model_io/models/
-  //  */
+   */
     const model = new ChatOpenAI({
       temperature: 0.7,
+      streaming: true,
+      callbacks: [
+        {
+          handleLLMNewToken(token) {
+            streamedResponse += token;
+          },
+        },
+      ],
 
       /**
        * @args
@@ -109,14 +133,25 @@ export async function POST(req: Request) {
       presencePenalty: 0.111,
       frequencyPenalty: 0.333,
       modelName: 'gpt-3.5-turbo-0301',
-      verbose:  true,
+      verbose:  false,
     });
 
-    /**
-     * Chat models stream message chunks rather than bytes, so this
-     * output parser handles serialization and encoding.
-     */
-    const outputParser = new BytesOutputParser();
+
+
+    /* Create the chain */
+const chain = ConversationalRetrievalQAChain.fromLLM(
+  model,
+  vectorStore.asRetriever()
+);
+
+
+
+/**
+ * Chat models stream message chunks rather than bytes, so this
+ * output parser handles serialization and encoding.
+ */
+const outputParser = new BytesOutputParser();
+const res = await chain.call({ question: currentMessageContent, chat_history: JSON.stringify(messages), });
 
     /*
     * Can also initialize as:
@@ -124,14 +159,20 @@ export async function POST(req: Request) {
     * import { RunnableSequence } from "langchain/schema/runnable";
     * const chain = RunnableSequence.from([prompt, model, outputParser]);
     */
-    const chain = prompt.pipe(model).pipe(outputParser);
+    // const chain = prompt.pipe(model).pipe(outputParser);
 
-    const stream = await chain.stream({
-      context: formattedContext,
-    });
+    // const stream = await chain.stream({
+    //   context: formattedContext,
+    // });
 
-    return new StreamingTextResponse(stream);
+    // return new StreamingTextResponse(res)
+    // return res;
 
+    console.log(streamedResponse)
+
+
+
+    return new StreamingTextResponse(streamedResponse as any);
     
 
 

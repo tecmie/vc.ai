753a5e2e68ed947f7d5936c99a761e6b9197c705
Andrew Miracle <me@andrewmiracle.com>
2023-09-15 21:03:06 +0000

    prompts and everything ready to go

--- app/api/chat/prompt.ts
+++ app/api/chat/prompt.ts
@@ -9,8 +9,22 @@ export const formatMessage = (message: VercelChatMessage) => {
   return `${message.role}: ${message.content}`;
 };
 
+
+
+/**
+ * Picks the last 3 items from an array if the array length is at least 3.
+ * @param {Array} array - The array to pick items from.
+ * @return {Array|null} - An array of the last 3 items or the last item if the array length is less than 3.
+ */
+export function lastSelectFromArray(array: VercelChatMessage[]) {
+  if (array.length < 3) {
+    return array.slice(0, -1);;
+  }
+  return array.slice(-3);
+}
+
 export const formattedPreviousMessages = (messages: VercelChatMessage[]) =>
-  messages.slice(0, -1).map(formatMessage);
+lastSelectFromArray(messages).map(formatMessage);
 
 export function formatContext(context: Record<string, unknown>[]) {
   // need to make sure our prompt is not larger than max size
@@ -20,17 +34,53 @@ export function formatContext(context: Record<string, unknown>[]) {
     .substring(0, 3750);
 }
 
+const __BASE_TEMPLATE__ = `
+You are an AI Assistant specialized in assisting Startup Founders, Entrepreneurs, and Small Business Owners. Your primary goal is to provide well-informed, actionable advice and responses based on the user's questions and context provided.
+
+MAIN CONTEXT:
+{main_context}
+
+PREVIOUS INTERACTIONS:
+{previous_interactions}
+
+QUESTION FROM USER:
+{user_question}
+
+Please use the main context and previous interactions to formulate an informative and appropriate response to the user's question. If the question is a greeting or a pleasantry, respond accordingly.
+
+RESPONSE:`;
+
 const __TEMPLATE__ = `
-You are a helpful Investment Banker who is always willing to share your knowledge with others.
+You are a Virtual Assistant to a Startup Founder/Entrepreneur. You have access to a broad database of investors across multiple sectors, including Government offices, accelerators, micro VCs, and angel investors.
 
 CONTEXT:
 
 {context}
 
-You have been given The above context based on a user question:
-Summarize and answer the question to the best of your ability.
-`;
+Based on the user's inquiry:
+- If it's a specific question related to investors or startup funding, consult the investor database and provide the most actionable and precise advice.
+- If the user inquires about other aspects of running a startup, such as marketing, tech, or operations, try to offer useful insights or suggestions.
+- If it's just a greeting or pleasantry, reciprocate with a polite and welcoming tone.
+
+USER QUESTION:
+
+{user_question}
+
+RESPONSE:`;
 
 export const prompt = PromptTemplate.fromTemplate<{
-  context: string;
-}>(__TEMPLATE__);
+  main_context: string;
+  previous_interactions?: string;
+  user_question: string;
+}>(__BASE_TEMPLATE__);
+
+
+/**
+ * Picks a random string from an array of strings.
+ * @param {string[]} stringArray - An array of strings to choose from.
+ * @return {string} - A random string picked from the array.
+ */
+ export function sampler(stringArray: string[]) {
+  const randomIndex = Math.floor(Math.random() * stringArray.length);
+  return stringArray[randomIndex];
+}
--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,16 +1,12 @@
 import { kv } from '@vercel/kv'
-import { OpenAIStream, StreamingTextResponse } from 'ai'
+import { StreamingTextResponse } from 'ai'
 import { Configuration, OpenAIApi } from 'openai-edge'
 
 import { nanoid } from '@/lib/utils'
-import { prompt } from './prompt';
+import { formattedPreviousMessages, prompt, sampler } from './prompt';
 import { connect, OpenAIEmbeddingFunction } from 'vectordb'
 import { ChatOpenAI } from 'langchain/chat_models/openai'
 import { BytesOutputParser } from 'langchain/schema/output_parser';
-import { LanceDB } from "langchain/vectorstores/lancedb";
-import { OpenAIEmbeddings } from "langchain/embeddings/openai";
-import { ConversationalRetrievalQAChain } from "langchain/chains";
-
 
 const embeddings = new OpenAIEmbeddingFunction('pageContent', process.env.OPENAI_API_KEY as string);
 
@@ -20,7 +16,7 @@ const session = {
     id: 'hack',
     name: 'Hack',
     email: 'hack@angel.com',
-}
+  }
 }
 
 // export const runtime = 'edge'
@@ -43,174 +39,99 @@ export async function POST(req: Request) {
 
   /* Store manager, pre generation */
 
-      const title = json.messages[0].content.substring(0, 100)
-      const id = json.id ?? nanoid()
-      const createdAt = Date.now()
-      const path = `/chat/${id}`
-      const payload = {
-        id,
-        title,
-        userId: session.user.id,
-        createdAt,
-        path,
-        messages
-      }
-      await kv.hmset(`chat:${id}`, payload)
-      await kv.zadd(`user:chat:${session.user.id}`, {
-        score: createdAt,
-        member: `chat:${id}`
-      })
+  const title = json.messages[0].content.substring(0, 100)
+  const id = json.id ?? nanoid()
+  const createdAt = Date.now()
+  const path = `/chat/${id}`
+  const payload = {
+    id,
+    title,
+    userId: session.user.id,
+    createdAt,
+    path,
+    messages
+  }
+  await kv.hmset(`chat:${id}`, payload)
+  await kv.zadd(`user:chat:${session.user.id}`, {
+    score: createdAt,
+    member: `chat:${id}`
+  })
 
   const currentMessageContent = messages[messages.length - 1].content;
 
-  
 
+  /**
+  * Vector Retrieval step for our actor
+  * We read the actor information from request cookie
+  * And use it to perform a similarity search on LanceDB
+  **/
+  const db = await connect('/Users/apple/srv/koolamusic/rag/data');
+  const table = await db.openTable('investors', embeddings);
 
-   /**
-   * Vector Retrieval step for our actor
-   * We read the actor information from request cookie
-   * And use it to perform a similarity search on LanceDB
-   **/
-    const db = await connect('/Users/apple/srv/koolamusic/rag/data');
-    // const table = await db.openTable('investors', embeddings);
-
-
-    const table = await db.openTable("investors");
-  
-    const vectorStore = new LanceDB(new OpenAIEmbeddings(), { table });
-  
-
-
-
-    /**
-     * We need to filter out the messages that are not tweets
-     * or quote tweets when executing our queries
-     *
-     * @see https://lancedb.github.io/lancedb/sql/
-     */
-    // const results = await table
-    //   .search(currentMessageContent)
-    //   .limit(3)
-    //   .execute();
-  
-    // // need to make sure our prompt is not larger than max size
-    // const formattedContext = results
-    //   .map((r) => r.pageContent)
-    //   .join('\n\n---\n\n')
-    //   .substring(0, 3750);
-  
-
-
-
-    //   console.log({
-    //     formattedContext,
-    //   })
-
-
-    let streamedResponse = "";
-
-    /**
-   * See a full list of supported models at:
-   * https://js.langchain.com/docs/modules/model_io/models/
+  /**
+   * We need to filter out the messages that are not tweets
+   * or quote tweets when executing our queries
+   *
+   * @see https://lancedb.github.io/lancedb/sql/
    */
-    const model = new ChatOpenAI({
-      temperature: 0.7,
-      streaming: true,
-      callbacks: [
-        {
-          handleLLMNewToken(token) {
-            streamedResponse += token;
-          },
-        },
-      ],
+  const results = await table
+    .search(currentMessageContent)
+    .limit(4)
+    .execute();
 
-      /**
-       * @args
-       * We use this to enable context in the user
-       * facing tweet generation input box
-       */
-      prefixMessages: messages,
-      presencePenalty: 0.111,
-      frequencyPenalty: 0.333,
-      modelName: 'gpt-3.5-turbo-0301',
-      verbose:  false,
-    });
+  // need to make sure our prompt is not larger than max size
+  const formattedContext = results
+    .map((r) => r.pageContent)
+    .join('\n\n---\n\n')
+    .substring(0, 3750);
 
 
 
-    /* Create the chain */
-const chain = ConversationalRetrievalQAChain.fromLLM(
-  model,
-  vectorStore.asRetriever()
-);
+
+  console.log({
+    formattedContext,
+  })
 
 
-
-/**
- * Chat models stream message chunks rather than bytes, so this
- * output parser handles serialization and encoding.
- */
-const outputParser = new BytesOutputParser();
-const res = await chain.call({ question: currentMessageContent, chat_history: JSON.stringify(messages), });
-
-    /*
-    * Can also initialize as:
-    *
-    * import { RunnableSequence } from "langchain/schema/runnable";
-    * const chain = RunnableSequence.from([prompt, model, outputParser]);
-    */
-    // const chain = prompt.pipe(model).pipe(outputParser);
-
-    // const stream = await chain.stream({
-    //   context: formattedContext,
-    // });
-
-    // return new StreamingTextResponse(res)
-    // return res;
-
-    console.log(streamedResponse)
-
-
-
-    return new StreamingTextResponse(streamedResponse as any);
+  /**
+* See a full list of supported models at:
+* https://js.langchain.com/docs/modules/model_io/models/
+//  */
+  const model = new ChatOpenAI({
+    temperature: 0.7,
     
 
+    /**
+     * @args
+     * We use this to enable context in the user
+     * facing tweet generation input box
+     */
+    prefixMessages: messages,
+    presencePenalty: 0.111,
+    frequencyPenalty: 0.333,
+    modelName: sampler(['gpt-3.5-turbo-0301', 'gpt-3.5-turbo-0301', 'gpt-4-0314']),
+    verbose: true,
+  });
 
+  /**
+   * Chat models stream message chunks rather than bytes, so this
+   * output parser handles serialization and encoding.
+   */
+  const outputParser = new BytesOutputParser();
 
-  // const res = await openai.createChatCompletion({
-  //   model: 'gpt-3.5-turbo',
-  //   messages,
-  //   temperature: 0.7,
-  //   stream: true
-  // })
+  /*
+  * Can also initialize as:
+  *
+  * import { RunnableSequence } from "langchain/schema/runnable";
+  * const chain = RunnableSequence.from([prompt, model, outputParser]);
+  */
+  const chain = prompt.pipe(model).pipe(outputParser);
 
-  // const stream = OpenAIStream(res, {
-  //   async onCompletion(completion) {
-  //     const title = json.messages[0].content.substring(0, 100)
-  //     const id = json.id ?? nanoid()
-  //     const createdAt = Date.now()
-  //     const path = `/chat/${id}`
-  //     const payload = {
-  //       id,
-  //       title,
-  //       userId: session.user.id,
-  //       createdAt,
-  //       path,
-  //       messages: [
-  //         ...messages,
-  //         {
-  //           content: completion,
-  //           role: 'assistant'
-  //         }
-  //       ]
-  //     }
-  //     await kv.hmset(`chat:${id}`, payload)
-  //     await kv.zadd(`user:chat:${session.user.id}`, {
-  //       score: createdAt,
-  //       member: `chat:${id}`
-  //     })
-  //   }
-  // })
+  const stream = await chain.stream({
+    main_context: formattedContext,
+    previous_interactions: formattedPreviousMessages(messages).join('\n'),
+    user_question: currentMessageContent,
+  });
 
-  // return new StreamingTextResponse(stream)
+  return new StreamingTextResponse(stream);
 }
